7e15d3867998a28fef27e2608cbeda3af35c2ed5
A7eXi <160401973+A7eXi@users.noreply.github.com>
2024-02-18 15:57:43 +0300

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>METANIT.COM</title>
+<meta charset="utf-8" />
+</head>
+<body>
+<h1>
+<?php
+echo "Основы синтаксиса";
+?>
+</h1>
+<div>
+<?php
+echo "<h2>Переменные</h2>";
+echo "Типы данных";
+?>
+</div>
+</body>
+</html>
